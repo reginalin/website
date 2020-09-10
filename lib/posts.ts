0f7a3e37c6298917c10a4f235c0d3c3858892eb1
Regina <reginalin714@gmail.com>
2020-09-09 20:53:42 -0400

    Fix typescript error.

--- lib/posts.ts
+++ lib/posts.ts
@@ -1,8 +1,8 @@
-import remark from "remark";
-import html from "remark-html";
 import fs from "fs";
 import path from "path";
 import matter from "gray-matter";
+import remark from "remark";
+import html from "remark-html";
 
 const postsDirectory = path.join(process.cwd(), "posts");
 
@@ -23,7 +23,7 @@ export function getSortedPostsData() {
     // Combine the data with the id
     return {
       id,
-      ...matterResult.data,
+      ...(matterResult.data as { date: string; title: string }),
     };
   });
   // Sort posts by date
@@ -38,7 +38,6 @@ export function getSortedPostsData() {
 
 export function getAllPostIds() {
   const fileNames = fs.readdirSync(postsDirectory);
-
   return fileNames.map((fileName) => {
     return {
       params: {
@@ -65,6 +64,6 @@ export async function getPostData(id: string) {
   return {
     id,
     contentHtml,
-    ...matterResult.data,
+    ...(matterResult.data as { date: string; title: string }),
   };
 }
