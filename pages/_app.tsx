446e27ea6db67710780ea56ed45cc8987c86ed2b
Regina <reginalin714@gmail.com>
2020-09-13 22:23:03 -0400

    Add google analytics.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,12 @@
+import { useEffect } from "react";
 import "../styles/global.css";
 import { AppProps } from "next/app";
+import ReactGA from "react-ga";
 
 export default function App({ Component, pageProps }: AppProps) {
+  useEffect(() => {
+    ReactGA.initialize("UA-177931764-1");
+    ReactGA.pageview(window.location.pathname + window.location.search);
+  }, []);
   return <Component {...pageProps} />;
 }
